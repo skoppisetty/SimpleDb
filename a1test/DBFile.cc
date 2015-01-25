b5f17e938e8669b9d4d16d363b0ecd9a3ef45b42
suresh <suresh.koppisetty@gmail.com>
2015-01-25 17:17:51 -0500

    added getnext and getnext literal

--- a1test/DBFile.cc
+++ a1test/DBFile.cc
@@ -21,39 +21,66 @@ int DBFile::Create (char *f_path, fType f_type, void *startup) {
 }
 
 void DBFile::Load (Schema &f_schema, char *loadpath) {
+	p.EmptyItOut();
+
 }
 
 int DBFile::Open (char *f_path) {
-	f.Open(1,f_path);
+	try{
+		f.Open(1,f_path);
+		return 1;
+	}
+	catch(...){
+		return 0;
+	}
+	
 }
 
 void DBFile::MoveFirst () {
+	p.EmptyItOut();
+	f.GetPage(&p,0);
 }
 
 int DBFile::Close () {
-	f.AddPage(&p, curpage);
-	f.Close();
+	try{
+		f.AddPage(&p, curpage);
+		p.EmptyItOut();
+		f.Close();
+		return 1;
+	}
+	catch(...){
+		return 0;
+	}
+
 }
 
 void DBFile::Add (Record &rec) {
-	cout << "Inside add" << "\n";
-	f.GetPage(&p,curpage);
-	cout << "after page" << "\n";
-	Schema mySchema ("catalog", "region");
-	rec.Print (&mySchema);
+	// Schema mySchema ("catalog", "region");p.Append(&rec)
+	// rec.Print (&mySchema);
 	if(!p.Append(&rec)){
-		cout << "new page" << "\n";
+		// cout << "new page" << "\n";
 		f.AddPage(&p, curpage);
 		p.EmptyItOut();
 		curpage++;totalpages++;
 		p.Append(&rec);
 	}
-
-	cout << "Done" << "\n";
+	// cout << "Done" << "\n";
 }
 
 int DBFile::GetNext (Record &fetchme) {
+	if(p.GetFirst(&fetchme)){
+		return 1;
+	}
+	else{
+		return 0;
+	}
 }
 
 int DBFile::GetNext (Record &fetchme, CNF &cnf, Record &literal) {
+	while(p.GetFirst(&fetchme)){
+		if (comp.Compare (&fetchme, &literal, &cnf)){
+			return 1;
+		}	
+	}
+	return 0;	
 }
