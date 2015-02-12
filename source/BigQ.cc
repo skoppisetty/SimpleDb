5fefe5348edbf37a6172f4d6bfed3ea359219cca
suresh <suresh.koppisetty@gmail.com>
2015-02-11 19:31:19 -0500

    Added code to BigQ just to consume records and push to consumer

--- source/BigQ.cc
+++ source/BigQ.cc
@@ -7,6 +7,36 @@ BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
  	// into the out pipe
 
     // finally shut down the out pipe
+
+	Record rec[2];
+	Record *last = NULL, *prev = NULL;
+	ComparisonEngine ceng;
+	int err = 0;
+	int i = 0;
+
+	while (in.Remove (&rec[i%2])) {
+		prev = last;
+		last = &rec[i%2];
+
+		if (prev && last) {
+			// if (ceng.Compare (prev, last, t->sortorder) == 1) {
+			// 	err++;
+			// }
+			out.Insert (prev);
+		}
+
+		i++;
+	}
+	out.Insert (last);
+
+	cout << " BigQ: removed " << i << " recs from the pipe\n";
+	cerr << " BigQ: " << (i - err) << " recs out of " << i << " recs in sorted order \n";
+	if (err) {
+		cerr << " BigQ: " <<  err << " recs failed sorted order test \n" << endl;
+	}
+
+
+
 	out.ShutDown ();
 }
 
